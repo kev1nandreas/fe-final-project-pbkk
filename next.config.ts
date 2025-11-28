42757cbb771f2d7659a006df14cada7c8205f9f8
Kevin Andreas <138026523+kev1nandreas@users.noreply.github.com>
2025-11-28 23:31:53 +0700

    fix: assetPrefix path for production environment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isProduction = process.env.NEXT_PUBLIC_RUN_MODE === "production";
 
 const nextConfig: NextConfig = {
-	assetPrefix: isProduction ? "/citation_checker/" : "",
+	assetPrefix: isProduction ? "/citation-checker/" : "",
 };
 
 export default nextConfig;
