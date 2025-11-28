6c6a63863b3051664fc2bae4433af9fb6f759bab
kev1nandreas <kevin.andreascn@gmail.com>
2025-11-28 10:04:37 +0700

    refactor: change base env

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -1,7 +1,11 @@
+const isDevelopment = process.env.NODE_ENV === "development";
+
 export const ENV = {
 	MODE: process.env.NEXT_PUBLIC_MODE || "development",
 	TOKEN_KEY: process.env.NEXT_PUBLIC_TOKEN_KEY || "@example/token",
 	URI: {
-		BASE_URL: process.env.NEXT_PUBLIC_API_URL_DEV || "http://localhost:3000",
+		BASE_URL: isDevelopment
+			? process.env.NEXT_PUBLIC_API_URL_DEV
+			: process.env.NEXT_PUBLIC_API_URL_PROD || "http://localhost:3000",
 	},
 };
