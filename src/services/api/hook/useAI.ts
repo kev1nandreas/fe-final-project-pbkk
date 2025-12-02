d9ba11ce25e031ee034703cc70c5c88bf65926fe
kev1nandreas <kevin.andreascn@gmail.com>
2025-12-02 19:09:43 +0700

    refactor: remove unused function

--- src/services/api/hook/useAI.ts
+++ src/services/api/hook/useAI.ts
@@ -1,57 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { typecastReferencesResponse } from "@/types/api";
-import { get, post } from "../main/call";
+import { get } from "../main/call";
 import { MAIN_ENDPOINT } from "../main/endpoint";
-// import { LLMRequestData } from "@/types/request";
-// import { typecastHistoryByIdResponse } from "@/types/response";
-
-// export const useSearchReferences = ({
-//   onSuccess,
-//   onError,
-// }: {
-//   onSuccess: (data: any) => void;
-//   onError: (error: any) => void;
-// }) => {
-//   return useMutation({
-//     mutationFn: async (body: LLMRequestData) => {
-//       const { Kind, OK } = await post(MAIN_ENDPOINT.Processing.Search, body);
-
-//       if (!OK) {
-//         throw new Error((Kind as { error: string }).error);
-//       }
-//       return Kind;
-//     },
-//     mutationKey: ["search.references"],
-//     onSuccess,
-//     onError,
-//   });
-// };
-
-// export const useFetchHistoryById = (
-//   id: string,
-//   onSuccess?: () => void,
-//   onError?: () => void
-// ) => {
-//   return useQuery({
-//     queryFn: async () => {
-//       const { Kind, OK } = await get(MAIN_ENDPOINT.Info.HistoryByID.replace(":id", id));
-//       if (!OK) {
-//         throw new Error(
-//           (Kind as { message: string }).message ||
-//             (Kind as { Message: string }).Message
-//         );
-//       }
-//       return (Kind as any).data;
-//     },
-//     queryKey: ["fetch.history.by.id", id],
-//   }) as any;
-// };
 
 export const useFetchHistory = (
-	onSuccess?: () => void,
-	onError?: () => void,
+	_onSuccess?: () => void,
+	_onError?: () => void,
 ) => {
 	return useQuery({
 		queryFn: async () => {
@@ -69,8 +23,8 @@ export const useFetchHistory = (
 };
 
 export const useFetchReferences = (
-	onSuccess?: () => void,
-	onError?: () => void,
+	_onSuccess?: () => void,
+	_onError?: () => void,
 ) => {
 	return useQuery({
 		queryFn: async () => {
