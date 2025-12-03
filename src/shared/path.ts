fc09be765d677b0fbb7706950c5acd1af3cd3679
mirai <mnabil190405@gmail.com>
2025-12-03 17:49:36 +0700

    fix: update basePath and assetPrefix in next.config.ts; simplify path constants in path.ts

--- src/shared/path.ts
+++ src/shared/path.ts
@@ -1,15 +1,10 @@
-import { ENV } from "@/configs/environment";
-
-const IsDevelopment = ENV.MODE === "development";
-const BASE_PATH = "/citation-checker";
-
 export const PATH = {
 	NOT_FOUND: "/404",
-	HOME: IsDevelopment ? "/" : `${BASE_PATH}/`,
-	RESULT: IsDevelopment ? "/results" : `${BASE_PATH}/results`,
-	START: IsDevelopment ? "/start" : `${BASE_PATH}/start`,
-	HISTORY: IsDevelopment ? "/history" : `${BASE_PATH}/history`,
-	REFERENCES: IsDevelopment ? "/references" : `${BASE_PATH}/references`,
+	HOME: "/",
+	RESULT: "/results",
+	START: "/start",
+	HISTORY: "/history",
+	REFERENCES: "/references",
 	PRIVATE: "/guard/ex",
-	ASSET_URL: IsDevelopment ? "" : `${BASE_PATH}`,
+	ASSET_URL: "",
 };
