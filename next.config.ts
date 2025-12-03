f072c8363ac148aa7d7dbc001dbe554519bafa9d
mirai <mnabil190405@gmail.com>
2025-12-03 18:03:57 +0700

    Revert "fix: update basePath and assetPrefix in next.config.ts; simplify path constants in path.ts"
    This reverts commit fc09be765d677b0fbb7706950c5acd1af3cd3679.

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const isProduction = process.env.NEXT_PUBLIC_RUN_MODE === "production";
 
 const nextConfig: NextConfig = {
-	basePath: isProduction ? "/citation-checker" : "",
-	assetPrefix: isProduction ? "/citation-checker" : "",
+	assetPrefix: isProduction ? "/citation-checker/" : "",
 	experimental: {
 		serverActions: {
 			bodySizeLimit: "50mb",
--- src/shared/path.ts
+++ src/shared/path.ts
@@ -1,10 +1,15 @@
+import { ENV } from "@/configs/environment";
+
+const IsDevelopment = ENV.MODE === "development";
+const BASE_PATH = "/citation-checker";
+
 export const PATH = {
 	NOT_FOUND: "/404",
-	HOME: "/",
-	RESULT: "/results",
-	START: "/start",
-	HISTORY: "/history",
-	REFERENCES: "/references",
+	HOME: IsDevelopment ? "/" : `${BASE_PATH}/`,
+	RESULT: IsDevelopment ? "/results" : `${BASE_PATH}/results`,
+	START: IsDevelopment ? "/start" : `${BASE_PATH}/start`,
+	HISTORY: IsDevelopment ? "/history" : `${BASE_PATH}/history`,
+	REFERENCES: IsDevelopment ? "/references" : `${BASE_PATH}/references`,
 	PRIVATE: "/guard/ex",
-	ASSET_URL: "",
+	ASSET_URL: IsDevelopment ? "" : `${BASE_PATH}`,
 };
