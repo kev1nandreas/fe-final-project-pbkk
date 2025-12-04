208fe1de972477cae0e389210fc0fcf8be80593f
kev1nandreas <kevin.andreascn@gmail.com>
2025-12-04 10:10:58 +0700

    chore: add new options

--- src/app/start/_containers/QueryBox.tsx
+++ src/app/start/_containers/QueryBox.tsx
@@ -24,7 +24,7 @@ export default function QueryBox({
 		defaultValues: {
 			query_text: "",
 			similarity_threshold: 0.5,
-			organization: "Lainnya",
+			organization: "Semuanya",
 		},
 	});
 
--- src/lib/options.ts
+++ src/lib/options.ts
@@ -1,7 +1,8 @@
 export const options = [
+	{ value: "Semuanya", label: "Semuanya" },
 	{
 		value: "Institut Teknologi Sepuluh Nopember",
-		label: "Institut Teknologi Sepuluh Nopember",
+		label: "Institut Teknologi Sepuluh Nopember (ITS)",
 	},
-	{ value: "Lainnya", label: "Lainnya" },
+	{ value: "Lainnya", label: "Non ITS" },
 ];
